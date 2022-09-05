9c65083c1f163171d23b823542b0768401f336a6
Carlos Corchado <carlos.garcia@wallia.io>
2022-09-05 14:35:26 -0500

    Estructura Base

---
+++ Z7M8C9A2/Z7M8C9A2 WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,26 @@
+//
+//  InterfaceController.swift
+//  Z7M8C9A2 WatchKit Extension
+//
+//  Created by Carlos Corchado on 05/09/22.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+    }
+
+}
---
+++ Z7M8C9A2/Z7M8C9A2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Z7M8C9A2
+//
+//  Created by Carlos Corchado on 05/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
