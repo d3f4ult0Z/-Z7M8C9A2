183adec68b70d2a01d19ec8099f80ac3060aa369
montse08vc <112901730+montse08vc@users.noreply.github.com>
2022-09-07 17:19:11 -0500

    Scroll View
    se agrega el scrollview en las vistas de registrar

--- Z7M8C9A2/Z7M8C9A2/Views/Register/RegisterViewController.swift
+++ Z7M8C9A2/Z7M8C9A2/Views/Register/RegisterViewController.swift
@@ -12,6 +12,7 @@ class RegisterViewController: UIViewController {
     
     @IBOutlet weak var btnRegistroFacebook: UIButton!
     @IBOutlet weak var btnRegistroGoogle: UIButton!
+    @IBOutlet weak var lblYaTienesCuenta: UILabel!
     @IBOutlet weak var btnRegistroFormulario: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,7 +41,7 @@ class RegisterViewController: UIViewController {
         
     }
 
-
+//    self.navigationController?.pushViewController(LoginViewController(), animated: true)
     @IBAction func btnRegistroApple(_ sender: Any) {
     }
     
